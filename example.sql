8e4bf3541153a785b7d6267c3828165219b1e75a
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-21 10:35:55 -0700

    code

---
+++ example.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE dogs_db;
+
+USE dogs_db;
+CREATE TABLE dogs (
+	name VARCHAR(25) NOT NULL,
+    age INT NOT NULL,
+    breed VARCHAR(30) NOT NULL
+);
+
+USE dogs_db;
+INSERT INTO dogs (name, age, breed)
+VALUES ('Bear', 6, 'Cockapoo'), ('Beef', 2, 'Chihuahua');
+
+USE dogs_db;
+SELECT * FROM dogs
+WHERE name = 'Beef';
